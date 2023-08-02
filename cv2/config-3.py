acaa178437409496b735b40693fb62f908d446be
rathaROG <siv.ratha89@gmail.com>
2023-08-02 15:03:29 +0200

    Improve config

--- cv2/config-3.py
+++ cv2/config-3.py
@@ -1,6 +1,8 @@
 # GitHub: https://github.com/rathaumons/opencv-for-pyppbox
 # Copyright (C) 2023 rathaROG
 
+import os
+
 PYTHON_EXTENSIONS_PATHS = [
     os.path.join(os.path.dirname(os.path.abspath(os.path.realpath(__file__))), 'python-3')
 ] + PYTHON_EXTENSIONS_PATHS
